5c04452482e7b620e236d2ee8e2212149d1c5480
Marlon Becker <marlon.mbs@gmail.com>
2021-02-10 16:00:05 +0100

    feat(setup): typescript, eslint, prettier config

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/_main.scss';
-
-ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/_main.scss'
+
+ReactDOM.render(
+  <React.StrictMode/>,
+  document.getElementById('root')
+)
